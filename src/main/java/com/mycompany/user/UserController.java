272d4d617e232b67466c5ca99dfd44f80a9617e5
Lingxiao Huang <jasonhlx1213@gmail.com>
2022-06-14 01:15:33 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/user/UserController.java
@@ -0,0 +1,63 @@
+package com.mycompany.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @Autowired private UserService service;
+
+    @GetMapping("/users")
+    public String showUserList(Model theModel) {
+        List<User> listUsers = service.listAll();
+        theModel.addAttribute("listUsers", listUsers);
+        return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String showNewForm(Model theModel) {
+        theModel.addAttribute("user", new User());
+        theModel.addAttribute("pageTitle", "Add New User");
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes ra) {
+        service.save(user);
+        ra.addFlashAttribute("message", "The user has been saved successfully.");
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public String showEditForm(@PathVariable("id") Integer id, Model theModel, RedirectAttributes ra) {
+        try {
+            User user = service.get(id);
+            theModel.addAttribute("user", user);
+            theModel.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+            return "user_form";
+        } catch (UserNotFoundException e) {
+            ra.addFlashAttribute("message", e.getMessage());
+            return "redirect:/users";
+        }
+    }
+
+    @GetMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable("id") Integer id, RedirectAttributes ra) {
+        try {
+            service.delete(id);
+            ra.addFlashAttribute("message", "The user with ID " + id + " has been deleted successfully.");
+        } catch (UserNotFoundException e) {
+            ra.addFlashAttribute("message", e.getMessage());
+        }
+        return "redirect:/users";
+    }
+
+}
